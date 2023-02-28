3bcc686f8062ee500733abbce3bb4a888a589bfc
Alberto <alberto.izquierdomo@gmail.com>
2023-02-28 17:04:30 +0100

    Create first component, ExpenseItem

--- Expensator/src/App.js
+++ Expensator/src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
-      <h2>Expensator</h2>
+      <h1>Expensator</h1>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ Expensator/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Date</h2>
+      <div>
+        <h4>Title</h4>
+        <p>Amount</p>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
