8f21028d3dae371aa8c6b3b41a3a2245831a5d9d
Alberto <alberto.izquierdomo@gmail.com>
2023-03-02 17:29:36 +0100

    Add props values to ExpenseItem and test it in App

--- Expensator/src/App.js
+++ Expensator/src/App.js
@@ -1,10 +1,52 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      date: new Date(2022, 12, 14),
+      icon: "Food",
+      description:"Bread",
+      type: "Groceries",
+      priceEuros: 2.17,
+      currency: "THB",
+      priceOther: 165.00
+    },
+    {
+      date: new Date(2022, 12, 13),
+      icon: "Train",
+      description:"Airport link",
+      type: "Transport",
+      priceEuros: 1.09,
+      currency: "THB",
+      priceOther: 40.00 
+    },
+    {
+      date: new Date(2022, 12, 13),
+      icon: "Train",
+      description:"Grab",
+      type: "Transport",
+      priceEuros: 1.77,
+      currency: "THB",
+      priceOther: 65.00
+    }
+  ]
+  
   return (
     <div>
       <h1>Expensator</h1>
-      <ExpenseItem />
+      {
+        expenses.map((expense, index) => (
+          <ExpenseItem
+            date={expense.date}
+            icon={expense.icon}
+            description={expense.description}
+            type={expense.type}
+            priceEuros={expense.priceEuros}
+            currency={expense.currency}
+            priceOther={expense.priceOther}
+          />
+        ))
+      }
     </div>
   );
 }
--- Expensator/src/components/ExpenseItem.js
+++ Expensator/src/components/ExpenseItem.js
@@ -1,32 +1,22 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 12, 14);
-  const expenseIcon = "Food";
-  const expenseDescription = "Bread";
-  const expenseType = "Groceries";
-  const expensePriceEuros = 2.17;
-  const expenseCurrency = "THB";
-  const expensePriceOther = 165.00;
-  
+export default function ExpenseItem(props) {  
   return (
     <div className="expense-item">
       <div className="expense-item__date">
-        <h2>{expenseDate.toDateString()}</h2>
+        <h2>{props.date.toDateString()}</h2>
       </div>
         <div className="expense-item__icon">
-          <p>{expenseIcon}</p>
+          <p>{props.icon}</p>
         </div>
         <div className="expense-item__description">
-          <h2>{expenseDescription}</h2>
-          <h4>{expenseType}</h4>
+          <h2>{props.description}</h2>
+          <h4>{props.type}</h4>
         </div>
       <div className="expense-item__price">
-        <p>{expensePriceEuros} €</p>
-        <p>{expensePriceOther} {expenseCurrency}</p>
+        <p>{props.priceEuros} €</p>
+        <p>{props.priceOther} {props.currency}</p>
       </div>
     </div>
   );
 }
-
-export default ExpenseItem;
