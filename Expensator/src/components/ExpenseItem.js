83c5a9440f1cf0933b83c766914b4ef2a9318a3e
Alberto <alberto.izquierdomo@gmail.com>
2023-03-02 16:58:51 +0100

    Add basic css to ExpenseItem. Add variables for information

--- Expensator/src/components/ExpenseItem.js
+++ Expensator/src/components/ExpenseItem.js
@@ -1,10 +1,29 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 12, 14);
+  const expenseIcon = "Food";
+  const expenseDescription = "Bread";
+  const expenseType = "Groceries";
+  const expensePriceEuros = 2.17;
+  const expenseCurrency = "THB";
+  const expensePriceOther = 165.00;
+  
   return (
-    <div>
-      <h2>Date</h2>
-      <div>
-        <h4>Title</h4>
-        <p>Amount</p>
+    <div className="expense-item">
+      <div className="expense-item__date">
+        <h2>{expenseDate.toDateString()}</h2>
+      </div>
+        <div className="expense-item__icon">
+          <p>{expenseIcon}</p>
+        </div>
+        <div className="expense-item__description">
+          <h2>{expenseDescription}</h2>
+          <h4>{expenseType}</h4>
+        </div>
+      <div className="expense-item__price">
+        <p>{expensePriceEuros} €</p>
+        <p>{expensePriceOther} {expenseCurrency}</p>
       </div>
     </div>
   );
