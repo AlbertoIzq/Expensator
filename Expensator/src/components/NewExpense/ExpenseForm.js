f0010b61bd37dd630fc408e9de7d470ea4d5d366
Alberto <alberto.izquierdomo@gmail.com>
2023-03-10 17:52:26 +0100

    Add form functionality to gather user input and store it.

--- Expensator/src/components/NewExpense/ExpenseForm.js
+++ Expensator/src/components/NewExpense/ExpenseForm.js
@@ -1,34 +1,101 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+    const [enteredDate, setEnteredDate] = useState("");
+    const [enteredDescription, setEnteredDescription] = useState("");
+    const [enteredType, setEnteredType] = useState("");
+    const [enteredPriceEuros, setEnteredPriceEuros] = useState("");
+    const [enteredCurrency, setEnteredCurrency] = useState("");
+    const [enteredPriceOther, setEnteredPriceOther] = useState("");
+    
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    }
+
+    const descriptionChangeHandler = (event) => {
+        setEnteredDescription(event.target.value);
+    }
+
+    const typeChangeHandler = (event) => {
+        setEnteredType(event.target.value);
+    }
+
+    const priceEurosChangeHandler = (event) => {
+        setEnteredPriceEuros(event.target.value);
+    }
+
+    const currencyChangeHandler = (event) => {
+        setEnteredCurrency(event.target.value);
+    }
+
+    const priceOtherChangeHandler = (event) => {
+        setEnteredPriceOther(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const enteredExpense =
+        {
+            date: new Date(enteredDate),
+            description: enteredDescription,
+            type: enteredType,
+            priceEuros: enteredPriceEuros,
+            currency: enteredCurrency,
+            priceOther: enteredPriceOther
+        }
+
+        console.log(enteredExpense);
+
+        // Reinitialize form
+        setEnteredDate("");
+        setEnteredDescription("");
+        setEnteredType("");
+        setEnteredPriceEuros("");
+        setEnteredCurrency("");
+        setEnteredPriceOther("");
+    }
+
     return (
-    <form>
+    <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
              <div className="new-expense__control">
                 <label>Date</label>
-                <input type='date' min="2019-01-01" step="2023-12-31" />
+                <input type='date' min="2019-01-01" step="2023-12-31"
+                value={enteredDate}
+                onChange={dateChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Description</label>
-                <input type='text' />
+                <input type='text'
+                value={enteredDescription}
+                onChange={descriptionChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Type</label>
-                <input type='text' />
+                <input type='text'
+                value={enteredType}
+                onChange={typeChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Price in €</label>
-                <input type='number' min="0.01" step="0.01" />
+                <input type='number' min="0.01" step="0.01"
+                value={enteredPriceEuros}
+                onChange={priceEurosChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Other currency</label>
-                <input type='text' />
+                <input type='text'
+                value={enteredCurrency}
+                onChange={currencyChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Price other currency</label>
-                <input type='number' min="0.01" step="0.01" />
+                <input type='number' min="0.01" step="0.01"
+                value={enteredPriceOther}
+                onChange={priceOtherChangeHandler}/>
             </div>
             
         </div>
